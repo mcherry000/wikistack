91ada4c49c9e8be6e7625221fd395a1881ca27d6
mcherry000 <cybermcherry000@gmail.com>
2020-06-25 14:38:43 -0400

    2.2 post lunch

--- app.js
+++ app.js
@@ -2,18 +2,37 @@ const express = require("express");
 const app = express();
 const morgan = require("morgan");
 const bodyParser = require("body-parser");
+const layout = require('./views/layout');
+const { db } = require('./models');
+
+
 
 app.use(morgan("dev"));
 app.use(express.static(__dirname + "/public"));
 app.use(bodyParser.urlencoded({ extended: false }));
 
 app.get("/", (req, res, next) => {
-  console.log("Hello sent");
-  res.send("Hello world!");
+  res.send(layout(''));
 });
 
-const PORT = 1337;
+app.get("/", (req, res, next) => {
+  res.send(
+
+  db.authenticate().
+  then(() => {
+  console.log('connected to the database');
+})
+});
+
+
+const PORT = 3000;
 
 app.listen(PORT, () => {
-  console.log(`App listening in port ${PORT}`);
+  console.log('whatever');
 });
+
+// const PORT = 1337;
+
+// app.listen(PORT, () => {
+//   console.log(`App listening in port ${PORT}`);
+// });
